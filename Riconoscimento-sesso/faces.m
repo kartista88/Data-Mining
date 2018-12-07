91d57510fe1812513d029eb811f8f19ff55e8d6f
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-12-07 22:59:02 +0100

    Aggiunto learning e validation set

--- Riconoscimento-sesso/faces.m
+++ Riconoscimento-sesso/faces.m
@@ -6,53 +6,49 @@ clc
 X = load('./NN_Datasets/face_x.txt');
 Y = load('./NN_Datasets/face_y.txt');
 
-prova = reshape(X(3,:),[60,60])';
-I = mat2gray(prova);
-imshow(I)
+%% Divido il dataset in LS e VS
+nl = round(.7 * size(X,1));
+i  = randperm(size(X,1));
+il = i(1:nl);
+iv = i(nl+1:end);
+
+XL = X(il,:);
+YL = Y(il);
+XV = X(iv,:);
+YV = Y(iv);
+
+clear X Y nl i il iv
 
 %% Inizializzo i parametri
-w = (2*rand(size(X,2),1)-1)';
+w = (2*rand(size(XL,2),1)-1)';
 b = 2*rand(1,1)-1;
 
-%% Algoritmo
-err = 0;
-for j=1:size(Y)
-    f = w*X(j,:)' + b;
-    if (Y(j)*f <= 0)
-        err = err + 1;
-    end    
-end
+%% Learning phase
 
-%%
+f = (w*XL')' + b;
+err = sum(YL.*f <= 0);
+
+%% Learning loop
+it = 0;
 i = 1;
-while (err > 0)
-    f = w*X(i,:)' + b;
-    if (Y(i)*f <= 0)
-        w = w + Y(i)*X(i,:);
-        b = b + Y(i);
-        err = 0;
-        for j=1:size(Y)
-            f = w*X(j,:)' + b;
-            if (Y(j)*f <= 0)
-                err = err + 1;
-            end
-        end
+while err > 0
+    f = w*XL(i,:)' + b;
+    if (YL(i)*f <= 0)
+        w = w + YL(i)*XL(i,:);
+        b = b + YL(i);
+        f = (w*XL')' + b;
+        err = sum(YL.*f <= 0);
     end
     i = i+1;
-    if (i > size(Y))
+    if (i > size(XL,1))
         i = 1;
     end
+    it = it + 1
 end
 
-res = zeros(size(Y));
-for i = 1:size(Y)
-    f = w*X(i,:)' + b;
-    if (f>=0)
-        res(i) = 1;
-    else
-        res(i) = -1;
-    end
-end
+%% Validation
+fV = (w*XV')' + b;
+err = sum(YV.*fV <= 0);
 
 
 
